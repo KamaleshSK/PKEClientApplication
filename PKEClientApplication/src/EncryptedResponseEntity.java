bd38f918b955bc483ab0bd9a42c73309ff4e09a0
therageofshiva <skamaleshfg@gmail.com>
2021-09-28 09:57:55 +0530

    Client application for the Public Key Encryption Implementation

---
+++ PKEClientApplication/src/EncryptedResponseEntity.java
@@ -0,0 +1,24 @@
+
+public class EncryptedResponseEntity {
+
+	private String RSAEncryptedPublicKey;
+	
+	private String AESEncryptedData;
+
+	public String getRSAEncryptedPublicKey() {
+		return RSAEncryptedPublicKey;
+	}
+
+	public void setRSAEncryptedPublicKey(String rSAEncryptedPublicKey) {
+		RSAEncryptedPublicKey = rSAEncryptedPublicKey;
+	}
+
+	public String getAESEncryptedData() {
+		return AESEncryptedData;
+	}
+
+	public void setAESEncryptedData(String aESEncryptedData) {
+		AESEncryptedData = aESEncryptedData;
+	}
+	
+}
